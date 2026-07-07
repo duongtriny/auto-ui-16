3fd13a41e25a24c3608ce8564c745bd841f2e9be
Duong Trinh <duong.triny@gmail.com>
2026-07-07 22:43:03 +0700

    [ID-03] Verify button

---
+++ tests/web-elements/elements/button.spec.ts
@@ -0,0 +1,17 @@
+import { expect, Page, test } from "@playwright/test";
+
+let inputs = ['Div button', 'Origin button', 'Input button', 'Default', 'Primary', 'Dashed', 'Text', 'Link', 'Icon button'];
+
+for (let input of inputs) {
+    test(`Verify button: ${input}]`, async ({ page }) => {
+        await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button');
+        await clickButtonByLabel(input, page);
+        await expect(page.getByText(`Button ${input} was clicked!`)).toBeVisible();
+    });
+}
+
+
+async function clickButtonByLabel(label: string, page: Page) {
+    let xpath = `//*[(@role='button' or self::button or self::input) and (normalize-space()='${label}' or @value='${label}')]`;
+    await page.locator(xpath).click();
+}
