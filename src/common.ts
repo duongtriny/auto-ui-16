49e0cced0fb01e602dc9927633f75bf28ae2d37e
Duong Trinh <duong.triny@gmail.com>
2026-08-14 21:23:00 +0700

    [ID-29] Verify create new product

--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,9 @@
 import { expect, Page } from "@playwright/test";
 
 export async function inputTextboxByLabel(label: string, input: string, page: Page) {
-    let xpath = `(//label[normalize-space()='${label}']/following::input)[1]`;
-    let inputLocator = page.locator(xpath);
+    let xpathTextbox = `(//label[normalize-space()='${label}']/following::input)[1]`;
+    let xpathTextarea = `(//label[normalize-space()='${label}']/following::textarea)[1]`;
+    let inputLocator = page.locator(`${xpathTextbox} | ${xpathTextarea}`).first();
     await inputLocator.click();
     await inputLocator.clear();
     await inputLocator.fill(input);
@@ -16,4 +17,23 @@ export async function clickButtonByLabel(label: string, page: Page) {
 export async function verifyFieldErrorMessageByLabel(label: string, message: string, page: Page) {
     let xpath = `(//label[normalize-space()='${label}']/following::div[contains(concat(' ', @class, ' '),' field-error ')and normalize-space()='${message}'])[1]`;
     await expect(page.locator(xpath)).toBeVisible();
+}
+
+export async function clickMenuItemByLabel(label: string, page: Page) {
+    let xpath = `//div[contains(concat(' ', @class, ' '), ' admin-navigation ')]//a[normalize-space()='${label}']`;
+    await page.locator(xpath).click();
+}
+
+export async function selectDropdownItemByLabel(label: string, item: string, page: Page) {
+    let xpath = `(//*[normalize-space()='${label}']/following::select)[1]`;
+    await page.locator(xpath).selectOption({ label: item });
+}
+
+export async function selectRadioButtonByLabel(label: string, option: string, page: Page) {
+    let xpath = `(//label[normalize-space()='${label}']/following::label[.//input[@type='radio'] and normalize-space()='${option}'])[1]`;
+    await page.locator(xpath).click();
+}
+
+export async function verifyNotificationMessage(message: string, page: Page) {
+    await expect(page.getByText(message)).toBeVisible();
 }
--- tests/ever-shop/product/new-product.spec.ts
+++ tests/ever-shop/product/new-product.spec.ts
@@ -1,16 +1,65 @@
 import { expect, Page, test } from "@playwright/test";
-import { clickButtonByLabel, inputTextboxByLabel } from "../../../src/common";
+import { clickButtonByLabel, clickMenuItemByLabel, inputTextboxByLabel, selectDropdownItemByLabel, selectRadioButtonByLabel, verifyNotificationMessage } from "../../../src/common";
 import { ADMIN_PASSWORD, ADMIN_USERNAME, URL } from "../../../src/utils/constants-utils";
+import path from "node:path";
 
 test.beforeEach(async ({ page }) => {
     await page.goto(URL);
 })
 
 test(`Verify create new product successful`, async ({ page }) => {
+    //Login
     await inputTextboxByLabel('Email', ADMIN_USERNAME, page);
     await inputTextboxByLabel('Password', ADMIN_PASSWORD, page);
     await clickButtonByLabel('SIGN IN', page);
+    //Verify user on Dashboard page
     let dashboardHeaderXpath = `//h1[contains(concat(' ', @class, ' '), ' page-heading-title ') and normalize-space()='Dashboard']`;
     await expect(page.locator(dashboardHeaderXpath)).toBeVisible();
+    await clickMenuItemByLabel('New Product', page);
+
+    //Verify user on New Product page
+    let newProductHeaderXpath = `//h1[contains(concat(' ', @class, ' '), ' page-heading-title ') and normalize-space()='Create a new product']`;
+    await expect(page.locator(newProductHeaderXpath)).toBeVisible();
+
+    //Input product's info
+    const random = new Date().getTime();
+    await inputTextboxByLabel('Name', `Iphone ${random}`, page);
+    await inputTextboxByLabel('SKU', `SKU-${random}`, page);
+    await inputTextboxByLabel('Price', '1500', page);
+    await inputTextboxByLabel('Weight', '0.05', page);
+    await selectCategory('Men', page);
+    await selectDropdownItemByLabel('Tax class', 'Taxable Goods', page);
+    await uploadProductImage('data/upload/iphone-17-pro-max-cam.jpg', page);
+    await selectRadioButtonByLabel('Status', 'Disabled', page);
+    await selectRadioButtonByLabel('Visibility', 'Not visible', page);
+    await selectRadioButtonByLabel('Manage stock?', 'No', page);
+    await selectRadioButtonByLabel('Stock availability', 'No', page);
+    await inputTextboxByLabel('Quantity', '100', page);
+    await selectDropdownItemByLabel('Attribute group', 'Default', page);
+    await selectDropdownItemByLabel('Color', 'Black', page);
+    await selectDropdownItemByLabel('Size', 'XXL', page);
+
+    await inputTextboxByLabel('Url key', `iphone-18-pro-max-${random}`, page);
+    await inputTextboxByLabel('Meta title', 'Iphone 18 Pro Max', page);
+    await inputTextboxByLabel('Meta keywords', 'Iphone 18, pro, max', page);
+    await inputTextboxByLabel('Meta description', 'Iphone 18 pro max description', page);
+    await clickButtonByLabel('Save', page);
+    // await page.waitForTimeout(1000);
+    await verifyNotificationMessage('Product saved successfully!', page);
 });
 
+async function selectCategory(category: string, page: Page) {
+    await page.getByRole('link', { name: 'Select category' }).click();
+    let searchCategoryInput = page.getByPlaceholder('Search categories');
+    await searchCategoryInput.clear();
+    await searchCategoryInput.click();
+    await searchCategoryInput.fill(category);
+    let selectButtonXpath = `(//h3[normalize-space()='${category}']/following::button[normalize-space()='Select'])[1]`;
+    await page.locator(selectButtonXpath).click();
+}
+
+async function uploadProductImage(filePath: string, page: Page) {
+    let selector = `#images input`;
+    let absolutePath = path.join(process.cwd(), filePath);
+    await page.locator(selector).setInputFiles(absolutePath);
+}
